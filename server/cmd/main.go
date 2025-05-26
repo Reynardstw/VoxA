bed8ffb447ccce3341055a65ef8282a04b8b4ba8
Admantix <williamwijaya499@gmail.com>
2025-05-26 18:11:42 +0700

    Implement database connection and migration functionality; add user and summary table migrations

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"os"
+	"server/infrastructure/db"
+
+	"github.com/redis/go-redis/v9"
+)
+
 func main() {
-	// initial entry
+	database := db.NewDbConnection()
+	defer database.Close()
+
+	db.Migrate(database, "up")
+	redisUrl := os.Getenv("REDIS_URL")
+	opt, err := redis.ParseURL(redisUrl)
+	if err != nil {
+		panic("Failed to parse Redis URL: " + err.Error())
+	}
+	rdb := redis.NewClient(opt)
+
+	defer rdb.Close()
 }
