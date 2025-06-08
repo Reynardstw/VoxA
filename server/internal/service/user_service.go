5efc7a5e4f933bb8f6dac25318a93e9c0c1e13ce
Admantix <williamwijaya499@gmail.com>
2025-06-08 20:35:40 +0700

    Add default profile URL to users table and remove unnecessary summary migration files

--- server/internal/service/user_service.go
+++ server/internal/service/user_service.go
@@ -103,8 +103,6 @@ func (s *UserServiceImpl) Login(ctx context.Context, request request.AuthRequest
 		return nil, fmt.Errorf("failed to find user: %w", err)
 	}
 
-	fmt.Print("User found: ", user.Email, "\n")
-	fmt.Print("User password: ", user.Password, "\n")
 	if user.Password != request.Password {
 		return nil, fmt.Errorf("invalid credentials")
 	}
